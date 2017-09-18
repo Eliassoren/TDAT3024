ad6f139bce2ce6b75ab31665fa2b3d11be61a806
Christian Echtermeyer <chrech.birr@gmail.com>
2017-09-18 20:28:25 +0200

    merge fix

--- main.m
+++ main.m
@@ -1,8 +1,3 @@
 
 % ([interval], initial conds, number of steps, steps per plot point, tolerance of error)  
-<<<<<<< HEAD
 tacoma([0 1000], [1 0 0.002 0], 25000, 5, 0.000001);
-=======
-%tacoma([0 1000], [1 0 0.001 0], 25000, 5, 0.01);
-traptacoma([0 1000], [1 0 0.001 0], 25000, 5);
->>>>>>> b35b8907d66c89c1c2804fc635b78483165e1ca4
